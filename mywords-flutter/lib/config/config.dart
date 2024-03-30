14dcff72714e58b9f4eb4e5320ea3b0831b1086d
vito-go <liushihao888@gmail.com>
2024-03-30 22:22:56 +0800

    add icon

--- mywords-flutter/lib/config/config.dart
+++ mywords-flutter/lib/config/config.dart
@@ -5,6 +5,7 @@ final Map<String, String> _hostIconsAssetPath = {
   "www.nytimes.com": "$_iconPrefix/nytimes.png",
   "www.economist.com": "$_iconPrefix/theeconomist.png",
   "www.cnbc.com": "$_iconPrefix/cnbc.png",
+  "www.nbcnews.com": "$_iconPrefix/cnbc.png",
   "www.chinadaily.com.cn": "$_iconPrefix/chinadaily.png",
   "www.bbc.com": "$_iconPrefix/bbc.png",
   "www.bbc.co.uk": "$_iconPrefix/bbc.png",
@@ -12,6 +13,11 @@ final Map<String, String> _hostIconsAssetPath = {
   "edition.cnn.com": "$_iconPrefix/cnn.png",
   "www.9news.com.au": "$_iconPrefix/9news.png",
   "www.washingtonpost.com": "$_iconPrefix/wp.png",
+  "www.foxnews.com": "$_iconPrefix/foxnews.png",
+  "apnews.com": "$_iconPrefix/ap.png",
+  "www.npr.org": "$_iconPrefix/ap.png",
+  "www.theguardian.com": "$_iconPrefix/theguardian.png",
+  "www.voanews.com": "$_iconPrefix/voanews.png",
 };
 
 String assetPathByHost(String host) {
