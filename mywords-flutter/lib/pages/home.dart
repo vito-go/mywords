083add71f6e39812e81473dfec76830359bd7018
liushihao <liushihao1993@hotmail.com>
2024-04-02 12:59:10 +0800

    v2.0.1 修复windows系统下，由于路径和其他系统标识符不一致,造成无法同步数据问题

--- mywords-flutter/lib/pages/home.dart
+++ mywords-flutter/lib/pages/home.dart
@@ -15,7 +15,7 @@ class Home extends StatefulWidget {
   State<Home> createState() => _State();
 }
 
-const appVersion = "2.0.0";
+const appVersion = "2.0.1";
 
 class _State extends State<Home> {
   final PageController _pageController =
