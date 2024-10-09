77003f4443cd0bfc78aa5beb52f9ff0b94dd6e85
vito <liushihao888@gmail.com>
2024-10-10 07:13:33 +0800

    share data

--- mywords-flutter/lib/pages/share_data.dart
+++ mywords-flutter/lib/pages/share_data.dart
@@ -119,14 +119,6 @@ class _SyncDataState extends State<SyncData> {
     List<Widget> children = [const PrivateIP()];
 
     children.add(ListTile(
-      leading: Tooltip(
-        // message:            "开启后将允许其他设备访问进行同步本机数据，也可以在浏览器中进行下载 http://ip:port/code,\n例如 http://$localExampleIP:${shareInfo.port}/${shareInfo.port}",
-        message:
-            "After opening, other devices will be allowed to access and synchronize local data, and you can also download it in the browser http://ip:port/code,\nfor example http://$localExampleIP:${shareInfo.port}/${shareInfo.port}",
-        triggerMode: TooltipTriggerMode.tap,
-        showDuration: const Duration(seconds: 30),
-        child: const Icon(Icons.info_outline),
-      ),
       title: Row(
         children: [
           Expanded(
@@ -167,11 +159,12 @@ class _SyncDataState extends State<SyncData> {
       trailing: switchBuild(),
     ));
     if (shareInfo.open) {
+      final hostname = handler.getHostName();
       final shareFileInfosURL =
-          "http://127.0.0.1:${shareInfo.port}/share/shareFileInfos?code=${shareInfo.code}";
+          "http://$hostname:${shareInfo.port}/share/shareFileInfos?code=${shareInfo.code}";
 
       children.add(ListTile(
-        title: const Text("文件列表"),
+        title: const Text("File List"),
         leading: const Icon(Icons.http),
         subtitle: Text(shareFileInfosURL),
         trailing: IconButton(
@@ -182,7 +175,7 @@ class _SyncDataState extends State<SyncData> {
       ));
 
       final shareKnownWordsURL =
-          "http://127.0.0.1:${shareInfo.port}/share/shareKnownWords?code=${shareInfo.code}";
+          "http://$hostname:${shareInfo.port}/share/shareKnownWords?code=${shareInfo.code}";
       children.add(ListTile(
         leading: const Icon(Icons.http),
         title: const Text("My Words Library"),
